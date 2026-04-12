527ca6c8932235d4afc35e214b80ed87d3393316
Rohitdev <rj8773458@gmail.com>
2026-04-12 14:22:56 +0530

    solve the question

---
+++ Variables_data-types_fundamentals/section-2.py
@@ -0,0 +1,93 @@
+# SECTION 2: GUESS THE OUTPUT (Explain Why) (11–20)
+
+# 1. 
+x=10
+y=5
+print(x+y)
+
+# What is the output? Why?
+# 1. 
+x=10
+y=3
+print(x/y)
+
+# What is the output and its data type?
+
+# 1. 
+
+# ```
+x=10
+y=3
+print(x//y)
+# ```
+
+# Explain the result.
+
+# 1. 
+
+# ```
+x="10"
+y="5"
+print(x+y)
+# ```
+
+# What is the output? Why?
+
+# 1. 
+
+# ```
+x="10"
+y=5
+print(x+str(y))
+# ```
+
+# What is the output? Why?
+
+# 1. 
+
+# ```
+x=2
+print(x**3)
+# ```
+
+# What does this print? Explain the operator.
+
+# 1. 
+
+# ```
+x="Hi"
+print(x*3)
+# ```
+
+# Explain why this works.
+
+# 1. 
+
+# ```
+x=5
+y=2.0
+print(x+y)
+# ```
+
+# What is the result and data type?
+
+# 1. 
+
+# ```
+x="100"
+print(int(x)+50)
+# ```
+
+# Explain step by step.
+
+# 1. 
+
+# ```
+x=5
+print("Value: ",(x))
+# ```
+
+# Why is str() needed?
+
+# ---
+
--- Variables_data-types_fundamentals/section-3.py
+++ Variables_data-types_fundamentals/section-3.py
@@ -1,30 +1,57 @@
-SECTION 3: PRACTICAL QUESTIONS (Write Code) (21–30)
+1# SECTION 3: PRACTICAL QUESTIONS (Write Code) (21–30)
 
-1. Create three variables: age (int), salary (float), name (str). Print all three.
-2. Take a string "50", convert it to integer, and add 20 to it.
-3. Create two float numbers and print their division result.
-4. Create a variable x = 25. Convert it into float and print its type.
-5. Create a variable price = 99.99. Convert it into int. Print result and explain what happened.
-6. Create a variable name = "Python". Print the first character.
-7. Create a string "100". Convert it to float, then multiply it by 2.
-8. Create two variables:
+# 1. Create three variables: age (int), salary (float), name (str). Print all three.
+
+name= "Rohit jatav"
+age= 19
+salary= 00
+print("name =",name)
+print("age =",age)
+print("salary =",salary)
+
+
+# 2. Take a string "50", convert it to integer, and add 20 to it.
+x="50"
+print(int(x)+20)
+
+# 3. Create two float numbers and print their division result.
+x=3.443
+y=4.09
+z=x/y
+print(z)
+
+# 4. Create a variable x = 25. Convert it into float and print its type.
+x=25
+print(type(float(x)))
+
+# 5. Create a variable price = 99.99. Convert it into int. Print result and explain what happened.
+price = 99.99
+x=(int(price))
+print(type(x),x)
+
+# 6. Create a variable name = "Python". Print the first character.
+name="python"
+print(name[0])
+# 7. Create a string "100". Convert it to float, then multiply it by 2.
+x="100"
+print(float(x)*2)
+# 8. Create two variables:    Print: Hello World
+a = "Hello"    
+b = "World"
+print(a,b)
+# 9. Create a variable x = 5. Print:
+#     The value of x is 5
+x=5
+print("The value of x is "+str(x))
+#     (Use type conversion properly.)
+
     
-    a = "Hello"
-    
-    b = "World"
-    
-    Print: Hello World
-    
-9. Create a variable x = 5. Print:
-    
-    The value of x is 5
-    
-    (Use type conversion properly.)
-    
-10. Create three variables:
-    
-    length = 5
-    
-    width = 2.5
-    
-    Compute area and print result. Also print its data type.
+# 10. Create three variables:
+#     length = 5
+#     width = 2.5
+#     Compute area and print result. Also print its data type.
+length=5
+width=2.5
+Area=length*width
+print(Area)
+print(type(Area))
